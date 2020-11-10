e19099ffc917fbd7a038fc46a86495ac30b02fcb
Marko Hietamäki <Marko.Hietamaki@seamk.fi>
2020-11-10 11:06:01 +0200

    Add project files.

---
+++ SocketPiirto1/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace SocketPiirto1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(121, 390);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(208, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.Color.White;
+            this.panel.Location = new System.Drawing.Point(121, 94);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(498, 251);
+            this.panel.TabIndex = 0;
+            this.panel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel;
+    }
+}
+
---
+++ SocketPiirto1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SocketPiirto1
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client;
+        private NetworkStream ns;
+        public Form1()
+        {
+            InitializeComponent();
+            connect();
+        }
+
+        private void panel_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point point = panel.PointToClient(Cursor.Position);
+            if (Control.MouseButtons == MouseButtons.Left)
+            {
+                textBox1.Text = point.ToString();
+                sentPosition(point.ToString());
+
+                SolidBrush brush = new SolidBrush(Color.Red);
+                panel.BringToFront();
+                Graphics g = panel.CreateGraphics();
+                g.FillEllipse(brush, point.X, point.Y, 4, 4);
+            }
+        }
+        private void connect()
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 5000;
+            client = new TcpClient();
+            client.Connect(ip, port);
+            Console.WriteLine("client connected!!");
+            ns = client.GetStream(); 
+        }
+        private void disconnect()
+        {
+            client.Client.Shutdown(SocketShutdown.Send);
+            ns.Close();
+            client.Close();
+            Console.WriteLine("disconnect from server!!");
+            Console.ReadKey();
+        }
+        private void sentPosition(string position)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(position);
+            if (buffer.Length > 0)
+            {
+                ns.Write(buffer, 0, buffer.Length);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
